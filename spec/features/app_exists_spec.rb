f24067636448c70eee9963d30c31b89f1fab0577
selenasmall <selenawiththetattoo@gmail.com>
2019-05-31 11:46:03 +1000

    Create React app  to be used within Rails
    Install webpacker so that your react app can run:
    https://github.com/rails/webpacker
    
    $ bundle exec rails webpacker:install
    
    Install react:
    $ bundle exec rails webpacker:install:react
    
    Test the installation worked correctly:
    - Run your test
    - Open the browser

--- spec/features/app_exists_spec.rb
+++ spec/features/app_exists_spec.rb
@@ -10,4 +10,14 @@ feature 'App exists', js: true do
       wait_for { page }.to have_content("Yay! You’re on Rails!")
     end
   end
+
+  scenario 'react app exists' do
+    When 'user visits the app' do
+      visit('/profile')
+    end
+
+    Then 'user sees they are on rails' do
+      wait_for { page }.to have_content('Hello React!')
+    end
+  end
 end
