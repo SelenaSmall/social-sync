b78a098168e07ece72604208d11eefae35d1595e
selenasmall <selenawiththetattoo@gmail.com>
2019-06-10 11:04:14 +1000

    No need to pass id

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -4,18 +4,11 @@ module Api
       @profiles = Profile.all
       render json: @profiles.each do |profile|
         {
-          id: profile.id,
           name: profile.name,
           url: profile.url,
           logo: profile.logo
         }
       end
-    end
-
-    private
-
-    def secure_params
-      params.require(:profile).permit(:id, :name, :url, :logo)
     end
   end
 end
